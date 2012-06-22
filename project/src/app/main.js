666c4df3bd770ba82e1b44bdcfc3535e7f4efd24
Mario Scheliga <mario@sourcegarden.de>
2012-06-22 23:22:08 +0200

    just aded sample setup for fakeserver and views

--- project/src/app/main.js
+++ project/src/app/main.js
@@ -1,4 +1,4 @@
-define(['text!tmpl/search.html'], function (tpl) {
+define(['text!tmpl/search.html', 'text!tmpl/resultitem.html'], function (tpl, itemtpl) {
     'use strict';
 
     console.log('am running', tpl);
@@ -9,6 +9,78 @@ define(['text!tmpl/search.html'], function (tpl) {
         url: '/api/search',
         model: SearchResultItem
     });
+
+    var SearchResultItemView = Backbone.View.extend({
+        initialize: function () {
+            this.template = doT.template(itemtpl);
+            this._modelBinder = new Backbone.ModelBinder();
+        },
+        render: function () {
+            this.$el.empty().append(this.template({}));
+            this._modelBinder.bind(this.model, this.el, Backbone.ModelBinder.createDefaultBindings(this.el, 'data-property'));
+            return this;
+        }
+    });
+
+    var SearchResultsView = Backbone.View.extend({
+        initialize: function () {
+            var viewCreator = function (model) {
+                return new SearchResultItemView({model: model});
+            };
+            var elManagerFactory = new Backbone.CollectionBinder.ViewManagerFactory(viewCreator);
+            this._collectionBinder = new Backbone.CollectionBinder(elManagerFactory);
+        },
+        render: function () {
+            this._collectionBinder.bind(this.collection, this.$el);
+            return this;
+        }
+    });
+
+    var SearchView = Backbone.View.extend({
+        className: 'searchview',
+        events: {
+            'click #gobutton': 'onClickGo',
+            'keydown': 'onKeyDown'
+        },
+        initialize: function () {
+            this.template = doT.template(tpl);
+            this.searchResults = new SearchResults();
+        },
+        render: function () {
+            this.$el.empty().append(this.template({}));
+            this.resultView = new SearchResultsView({collection: this.searchResults, el: this.$('#searchresults')});
+            this.resultView.render();
+
+            return this;
+        },
+        onClickGo: function (e) {
+            this.triggerSearch();
+        },
+        onKeyDown: function (e) {
+            switch(e.which) {
+            case 13:
+                this.triggerSearch();
+                break;
+            }
+        },
+        triggerSearch: function () {
+            var val = this.$('#searchinput').val();
+            this.$('#searchinput').val('');
+            if (val !== '') {
+                this.doSearch(val);
+            }
+
+        },
+        doSearch: function (query) {
+            console.log('do search:' + query);
+            this.searchResults.fetch({query: query});
+        }
+    });
+
+
+    var view = new SearchView();
+    $('#main.container').empty().append(view.render().el);
+    $('#searchinput').focus();
 
     window.col = new SearchResults();
 
--- project/src/dev/fakeserver.js
+++ project/src/dev/fakeserver.js
@@ -1,12 +1,94 @@
-// fake server definitions follow here
-define(['text!dev/responses/searchresults.json'], function (searchResults) {
+require(['text!dev/responses/searchresults.json'], function (searchResults) {
+
     'use strict';
 
-    var fakeserver = sinon.fakeServerWithClock.create();
+    var fakeserver;
 
-    fakeserver.autoRespondAfter = 1;
-    fakeserver.respondWith("GET", '/api/search', [200, {'Content-Type': 'application/json'}, searchResults]);
+    function setupFakeServer() {
+        fakeserver = sinon.fakeServer.create();
 
-    window.fakeserver = fakeserver;
-    return fakeserver;
+        fakeserver.autoRespond = true;
+        fakeserver.autoRespondAfter = 1;
+        fakeserver.respondWith("GET", '/api/search', function (xhr) {
+            xhr.respond(200, {'Content-Type': 'application/json'}, searchResults);
+        });
+    }
+
+    function restoreServer() {
+        fakeserver.restore();
+    }
+
+
+
+
+
+    // Helper function to get a value from a Backbone object as a property
+    // or as a function.
+    var getValue = function(object, prop) {
+        if (!(object && object[prop])) return null;
+        return _.isFunction(object[prop]) ? object[prop]() : object[prop];
+    };
+
+    // Throw an error when a URL is needed, and none is supplied.
+    var urlError = function() {
+        throw new Error('A "url" property or function must be specified');
+    };
+
+    var methodMap = {
+        'create': 'POST',
+        'update': 'PUT',
+        'delete': 'DELETE',
+        'read':   'GET'
+    };
+
+    Backbone.sync = function (method, model, options) {
+        var type = methodMap[method];
+        options || (options = {});
+
+        // Default JSON-request options.
+        var params = {type: type, dataType: 'json'};
+
+        if (!options.url) {
+          params.url = getValue(model, 'url') || urlError();
+        }
+
+        // Ensure that we have the appropriate request data.
+        if (!options.data && model && (method == 'create' || method == 'update')) {
+            params.contentType = 'application/json';
+            params.data = JSON.stringify(model.toJSON());
+        }
+
+        // For older servers, emulate JSON by encoding the request into an HTML-form.
+        if (Backbone.emulateJSON) {
+            params.contentType = 'application/x-www-form-urlencoded';
+            params.data = params.data ? {model: params.data} : {};
+        }
+
+        // For older servers, emulate HTTP by mimicking the HTTP method with `_method`
+        // And an `X-HTTP-Method-Override` header.
+        if (Backbone.emulateHTTP) {
+            if (type === 'PUT' || type === 'DELETE') {
+                if (Backbone.emulateJSON) params.data._method = type;
+                params.type = 'POST';
+                params.beforeSend = function(xhr) {
+                    xhr.setRequestHeader('X-HTTP-Method-Override', type);
+                };
+            }
+        }
+
+        // Don't process data on a non-GET request.
+        if (params.type !== 'GET' && !Backbone.emulateJSON) {
+            params.processData = false;
+        }
+
+        setupFakeServer();
+        // Make the request, allowing the user to override any Ajax options.
+        return $.ajax(_.extend(params, options)).pipe(function (data) {
+            restoreServer();
+            return data;
+        })
+        .fail(function () {
+            console.log('ERROR', arguments);
+        });
+    };
 });
